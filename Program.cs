c2765c72fbfc05b88b334a039c682dc3ff27763f
falau <falau@LENOVO>
2024-12-13 16:54:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Set2Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine()) ;
+            }
+
+            int max = v.Max();
+            int min = v.Min();
+            Console.WriteLine($"Max = {max}, min={min}");
+        }
+    }
+}
